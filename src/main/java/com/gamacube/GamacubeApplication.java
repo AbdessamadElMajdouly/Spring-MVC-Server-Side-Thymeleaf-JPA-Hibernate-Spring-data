f564eb0f5e637e6d17d4e94d8a9a56466f7131d3
Abdessamad El Majdouly <elmajdouly.abdessamad@gmail.com>
2020-02-10 13:32:59 +0100

    initial commit

---
+++ src/main/java/com/gamacube/GamacubeApplication.java
@@ -0,0 +1,13 @@
+package com.gamacube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamacubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamacubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamacube/GamacubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamacube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamacubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
